449e618ea5d867e119730b6174a38bfc983af8da
suetamsoft7-hash <suetamsoft7@gmail.com>
2026-03-30 15:46:05 -0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "favicon.png": "22f5f7638abe8d981526839876401490",
 "firebase-messaging-sw.js": "4cf1479b0089d2507ac8a65e4ef1e7e6",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "3e370bc50dd42efd6a3bfe9a0e185f49",
+"flutter_bootstrap.js": "1e550124c03d36047af45e6b56d021fe",
 "icons/Icon-192.png": "2032caf7e7c4e560babe4ff8226a1336",
 "icons/Icon-512.png": "c4131f367c06083f6253e3d791dcb8dd",
 "icons/Icon-maskable-192.png": "c4131f367c06083f6253e3d791dcb8dd",
