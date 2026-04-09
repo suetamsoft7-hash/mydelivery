12047e92eab659a751852d8a63bd125003910499
suetamsoft7-hash <suetamsoft7@gmail.com>
2026-04-09 13:22:23 -0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"64e1e4311ed17c845ff1badb085db257d07a8c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "939563332"
+    serviceWorkerVersion: "3813075953"
   }
 });
